db075e58709d49ecd82d02ad7e46d59de0b0994d
Rakib86547 <contact.antor.bd@gmail.com>
2022-11-24 14:34:07 +0600

    get jeeps data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD)
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tiiizrg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoriesCollection = client.db('carMaster').collection('categories');
+
+        app.get('/jeeps/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { category_id: id };
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error));
 
 app.get('/', (req, res) => {
     res.send('api is running')
