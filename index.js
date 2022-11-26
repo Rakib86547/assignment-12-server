0fd0a4dde2b9d3b8a5e5f754f155991083303863
Rakib86547 <contact.antor.bd@gmail.com>
2022-11-26 07:27:07 +0600

    jwt create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tiiizrg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify jwt function
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(403).send({message: "unauthorized access"});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_JWT_TOKEN, function(err, decoded) {
+        if(err) {
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         const categoriesCollection = client.db('carMaster').collection('categories');
@@ -38,6 +54,17 @@ async function run() {
             const users = req.body;
             const result = await usersCollection.insertOne(users);
             res.send(result)
+        });
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = {email:email};
+            const user = await usersCollection.findOne(query);
+            if(user) {
+                const token = jwt.sign({email}, process.env.ACCESS_JWT_TOKEN, {expiresIn: '1d'});
+                return res.send({accessToken: token})
+            }
+            res.status(401).send({accessToken: ''});
         })
 
     }
